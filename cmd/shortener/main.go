8b208c3826d4e52c55e2853589c0e253d625a812
a.demin <a.demin@bars.group>
2026-02-23 13:20:54 +0700

    increment 1 refactoring

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,58 +1,15 @@
 package main
 
 import (
-	"io"
-	"log"
 	"net/http"
+
+	"github.com/AlexeyD1982/shortener/internal/handler"
 )
-
-var url string
-
-func postEndpoint(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Wrong request", http.StatusBadRequest)
-		return
-	}
-
-	if r.Header.Get("content-type") != "text/plain" {
-		http.Error(w, "Wrong request", http.StatusBadRequest)
-		return
-	}
-
-	if r.URL.Path != "/" {
-		http.Error(w, "Wrong request", http.StatusBadRequest)
-		return
-	}
-
-	content, err := io.ReadAll(r.Body)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	url = string(content)
-	w.WriteHeader(http.StatusCreated)
-	_, err = w.Write([]byte("http://localhost:8080/EwHXdJfB"))
-
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func getEndpoint(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Wrong request", http.StatusBadRequest)
-		return
-	}
-
-	w.Header().Set("location", url)
-	w.WriteHeader(http.StatusTemporaryRedirect)
-}
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", postEndpoint)
-	mux.HandleFunc("/EwHXdJfB", getEndpoint)
+	mux.HandleFunc("/", handler.SetUrlHandler)
+	mux.HandleFunc("/EwHXdJfB", handler.GetUrlHandler)
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		panic(err)
---
+++ internal/handler/routing.go
@@ -0,0 +1,50 @@
+package handler
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+var url string
+
+func GetUrlHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Wrong request", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("location", url)
+	w.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+func SetUrlHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Wrong request", http.StatusBadRequest)
+		return
+	}
+
+	if r.Header.Get("content-type") != "text/plain" {
+		http.Error(w, "Wrong request", http.StatusBadRequest)
+		return
+	}
+
+	if r.URL.Path != "/" {
+		http.Error(w, "Wrong request", http.StatusBadRequest)
+		return
+	}
+
+	content, err := io.ReadAll(r.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	url = string(content)
+	w.WriteHeader(http.StatusCreated)
+	_, err = w.Write([]byte("http://" + r.Host + "/EwHXdJfB"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
