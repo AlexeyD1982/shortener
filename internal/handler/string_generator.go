a3f14c5209f1dd85c881b70203aa40af2ec8bb60
a.demin <a.demin@bars.group>
2026-02-26 21:26:49 +0700

    increment 1 generate random id

---
+++ internal/handler/string_generator.go
@@ -0,0 +1,19 @@
+package handler
+
+import (
+	"math/rand"
+	"time"
+)
+
+func generateRandomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+	seed := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(seed)
+
+	result := make([]byte, length)
+	for i := range result {
+		result[i] = charset[random.Intn(len(charset))]
+	}
+	return string(result)
+}
