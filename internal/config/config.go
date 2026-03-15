28a11f406b2500d2a29274caa85e4c9b5fb12303
a.demin <a.demin@bars.group>
2026-03-15 09:22:15 +0700

    increment 5

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"net/url"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -18,37 +19,53 @@ type Conf struct {
 }
 
 func Get(withFlags bool) *Conf {
+	var haveHostEnv, haveResultHostEnv bool
+
 	conf := Conf{
 		Host:       defaultHost,
 		ResultHost: defaultResultHost,
 	}
 
+	if hostEnv := os.Getenv("SERVER_ADDRESS"); hostEnv != "" {
+		conf.Host = hostEnv
+		haveHostEnv = true
+	}
+
+	if resultHostEnv := os.Getenv("SERVER_ADDRESS"); resultHostEnv != "" {
+		conf.Host = resultHostEnv
+		haveResultHostEnv = true
+	}
+
 	if withFlags {
-		flag.Func("a", "HTTP server URL", func(flagValue string) error {
-			urlParts := strings.Split(flagValue, ":")
-			if len(urlParts) != 2 {
-				return errors.New("invalid URL format")
-			}
-			if _, err := strconv.Atoi(urlParts[1]); err != nil {
-				return fmt.Errorf("invalid port: %s", flagValue)
-			}
+		if !haveHostEnv {
+			flag.Func("a", "HTTP server URL", func(flagValue string) error {
+				urlParts := strings.Split(flagValue, ":")
+				if len(urlParts) != 2 {
+					return errors.New("invalid URL format")
+				}
+				if _, err := strconv.Atoi(urlParts[1]); err != nil {
+					return fmt.Errorf("invalid port: %s", flagValue)
+				}
 
-			conf.Host = flagValue
-			return nil
-		})
+				conf.Host = flagValue
+				return nil
+			})
+		}
 
-		flag.Func("b", "result short URL", func(flagValue string) error {
-			u, err := url.Parse(flagValue)
-			if err != nil {
-				return errors.New("invalid Result URL format")
-			}
+		if !haveResultHostEnv {
+			flag.Func("b", "result short URL", func(flagValue string) error {
+				u, err := url.Parse(flagValue)
+				if err != nil {
+					return errors.New("invalid Result URL format")
+				}
 
-			if u.Hostname() == "" {
-				return errors.New("invalid Result URL format")
-			}
-			conf.ResultHost = flagValue
-			return nil
-		})
+				if u.Hostname() == "" {
+					return errors.New("invalid Result URL format")
+				}
+				conf.ResultHost = flagValue
+				return nil
+			})
+		}
 
 		flag.Parse()
 	}
